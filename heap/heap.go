4955ed7e3dc7560633b1e47b2e565320ccfaae06
ppp <wandererswordsman@gmail.com>
2019-06-04 08:36:11 +0800

    [add]:heap ,add "pop the top index"

--- heap/heap.go
+++ heap/heap.go
@@ -5,10 +5,10 @@ import (
 	"math"
 )
 
-type heap struct {
-	items []interface{}
-	/*left is bigger than right*/
-	whetherIndexIsBiggerOrEqual func(other, index interface{}) bool
+type Heap struct {
+	items                       []interface{}
+	heapSize                    int
+	whetherIndexIsBiggerOrEqual func(other, root interface{}) bool
 }
 
 func leftIndex(i int) int {
@@ -23,53 +23,75 @@ func parentIndex(i int) int {
 	return (i - 1) / 2
 }
 
-func (h *heap) heapSize() int {
-	return len(h.items)
+func NewHeap(items []interface{}, compare func(other, root interface{}) bool) Heap {
+	h := Heap{}
+	h.items = items
+	h.heapSize = len(items)
+	h.whetherIndexIsBiggerOrEqual = compare
+	h.buildHeap()
+	return h
 }
 
-func (h *heap) heapify(index int) {
-	if index >= h.heapSize() {
+func (h *Heap) top() interface{} {
+	if len(h.items) == 0 {
+		return nil
+	}
+	return h.items[0]
+}
+
+func (h *Heap) popTop() interface{} {
+	if len(h.items) == 0 {
+		return nil
+	}
+	rt := h.items[0]
+	h.items[0] = h.items[h.heapSize-1]
+	h.items[h.heapSize-1] = nil
+	h.heapSize -= 1
+	h.heapify(0)
+	return rt
+}
+
+func (h *Heap) heapify(index int) {
+	if index >= h.heapSize {
 		return
 	}
 	cItem := h.items[index]
 	lIndex := leftIndex(index)
 	rIndex := rightIndex(index)
-	if lIndex < h.heapSize() {
-		lItem := h.items[lIndex]
-		largestIndex := index
-		if h.whetherIndexIsBiggerOrEqual(lItem, cItem) == false {
-			largestIndex = lIndex
-		}
-		if rIndex < h.heapSize() {
-			rItem := h.items[rIndex]
-			if h.whetherIndexIsBiggerOrEqual(rItem, h.items[largestIndex]) == false {
-				largestIndex = rIndex
-			}
-		}
-		if largestIndex != index {
-			h.items[index], h.items[largestIndex] = h.items[largestIndex], h.items[index]
-			h.heapify(largestIndex)
-		}
+	if lIndex >= h.heapSize {
+		return
+	}
+	largestIndex := index
+	if h.whetherIndexIsBiggerOrEqual(h.items[lIndex], cItem) == false {
+		largestIndex = lIndex
+	}
+	if rIndex < h.heapSize &&
+		h.whetherIndexIsBiggerOrEqual(h.items[rIndex], h.items[largestIndex]) == false {
+		largestIndex = rIndex
+	}
+	if largestIndex != index {
+		h.items[index], h.items[largestIndex] = h.items[largestIndex], h.items[index]
+		h.heapify(largestIndex)
 	}
 }
 
-func (h *heap) buildHeap() {
-	i := h.heapSize() / 2
+func (h *Heap) buildHeap() {
+	i := h.heapSize / 2
 	for index := i; index >= 0; index-- {
 		h.heapify(index)
 	}
 }
 
-func (h *heap) check() bool {
+func (h *Heap) check() bool {
 	for i, v := range h.items {
 		lIndex := leftIndex(i)
 		rIndex := rightIndex(i)
-		if lIndex < h.heapSize() {
+		if lIndex < h.heapSize {
 			if h.whetherIndexIsBiggerOrEqual(h.items[lIndex], v) == false {
 				return false
 			}
 		}
-		if rIndex < h.heapSize() {
+		if rIndex < h.heapSize {
 			if h.whetherIndexIsBiggerOrEqual(h.items[rIndex], v) == false {
 				return false
 			}
@@ -78,17 +100,17 @@ func (h *heap) check() bool {
 	return true
 }
 
-func (h *heap) print() {
+func (h *Heap) print() {
 	bounds := 2
 
-	rows := int(math.Ceil(math.Log2(float64(h.heapSize())))) - 1
+	rows := int(math.Ceil(math.Log2(float64(h.heapSize)))) - 1
 	rows = int(math.Pow(2, float64(rows)))
 
-	for index, value := range h.items {
+	for index := 0; index < h.heapSize; index++ {
 		for i := 0; i < int(rows); i++ {
 			fmt.Printf(" ")
 		}
-		fmt.Printf("%d", value)
+		fmt.Printf("%d", h.items[index])
 		if index == bounds-2 {
 			fmt.Printf("\n")
 			bounds = bounds * 2
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -1,6 +1,7 @@
 package heap
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -35,8 +36,9 @@ func expect(a, b int, t *testing.T) {
 }
 
 func TestMaxHeapify(t *testing.T) {
-	h := &heap{}
-	h.items = []interface{}{1, 2, 3, 4, 5, 6, 7}
+	h := &Heap{}
+	h.items = []interface{}{1, 2, 3, 4}
+	h.heapSize = len(h.items)
 	//this is a maxHeap
 	h.whetherIndexIsBiggerOrEqual = func(other, index interface{}) bool {
 		itemValue := other.(int)
@@ -54,8 +56,9 @@ func TestMaxHeapify(t *testing.T) {
 }
 
 func TestMinHeapify(t *testing.T) {
-	h := &heap{}
-	h.items = []interface{}{7, 6, 5, 4, 3, 2, 1}
+	h := &Heap{}
+	h.items = []interface{}{7, 6, 5, 4}
+	h.heapSize = len(h.items)
 	//this is a maxHeap
 	h.whetherIndexIsBiggerOrEqual = func(other, index interface{}) bool {
 		itemValue := other.(int)
@@ -66,6 +69,19 @@ func TestMinHeapify(t *testing.T) {
 	h.heapify(0)
 	h.print()
 	h.buildHeap()
+	h.print()
+	if h.check() == false {
+		t.Fail()
+	}
+}
+
+func TestPop(t *testing.T) {
+	h := NewHeap([]interface{}{1, 2, 3, 4, 5, 4, 3, 2, 1}, func(other, root interface{}) bool {
+		return root.(int) > other.(int)
+	})
+	h.print()
+	top := h.popTop()
+	fmt.Printf("pop top:%d\n", top)
 	h.print()
 	if h.check() == false {
 		t.Fail()
